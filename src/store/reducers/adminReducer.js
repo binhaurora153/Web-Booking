73a3dc2768988240bd7be730b6f4132624116a17
Thiện Bình <21t1020254@husc.edu.vn>
2024-09-06 21:00:02 +0700

    add new redux

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -1,27 +1,52 @@
 import actionTypes from "../actions/actionTypes";
 
 const initialState = {
+  isLoadingGender: false,
   genders: [],
-  role: [],
-  position: [],
+  roles: [],
+  positions: [],
 };
 
 const adminReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.FETCH_GENDER_START:
-      console.log("fire ", action);
-      return {
-        ...state,
-      };
-    case actionTypes.FETCH_GENDER_SUCCESS:
       let copyState = { ...state };
-      copyState.genders = action.data;
-      console.log("firesucces ", copyState);
+      copyState.isLoadingGender = true;
       return {
         ...copyState,
       };
+
+    case actionTypes.FETCH_GENDER_SUCCESS:
+      state.genders = action.data;
+      state.isLoadingGender = false;
+      return {
+        ...state,
+      };
     case actionTypes.FETCH_GENDER_FAILED:
-      console.log("firefaifed ", action);
+      state.isLoadingGender = false;
+      state.genders = [];
+      return {
+        ...state,
+      };
+    //
+    case actionTypes.FETCH_POSITION_SUCCESS:
+      state.positions = action.data;
+      return {
+        ...state,
+      };
+    case actionTypes.FETCH_POSITION_FAILED:
+      state.positions = [];
+      return {
+        ...state,
+      };
+    //
+    case actionTypes.FETCH_ROLE_SUCCESS:
+      state.roles = action.data;
+      return {
+        ...state,
+      };
+    case actionTypes.FETCH_ROLE_FAILED:
+      state.roles = [];
       return {
         ...state,
       };
