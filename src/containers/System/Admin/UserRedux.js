82b78f556fd5adc5922a9b116700510b97e75668
Thiện Bình <21t1020254@husc.edu.vn>
2024-09-06 14:58:18 +0700

    form redux

---
+++ src/containers/System/Admin/UserRedux.js
@@ -0,0 +1,174 @@
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import { getAllCodeService } from "../../../services/userService";
+import { LANGUAGES } from "../../../utils/constant";
+
+import * as actions from "../../../store/actions";
+class UserRedux extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      genderArr: [],
+    };
+  }
+
+  async componentDidMount() {
+    this.props.getGenderStart();
+    // try {
+    //   let res = await getAllCodeService("gender");
+    //   if (res && res.errCode === 0) {
+    //     this.setState({
+    //       genderArr: res.data,
+    //     });
+    //   }
+    // } catch (e) {
+    //   console.log(e);
+    // }
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    //render =>didupdate
+    // hiện tại(this) và quá khứ(prev)
+    //qua khứ [] hiện tại[3];
+    if (prevProps.genderRedux !== this.props.genderRedux) {
+      this.setState({
+        genderArr: this.props.genderRedux,
+      });
+    }
+  }
+  render() {
+    let genders = this.state.genderArr;
+    let language = this.props.language;
+    console.log("check prop reudx", this.props.genderRedux);
+    return (
+      <div className="user-redux-container">
+        <div
+          className="title mt-4 mb-4"
+          style={{
+            background: "linear-gradient(to right, gray 10%, #330867 100%)",
+            backgroundClip: "text",
+            color: "transparent",
+          }}
+        >
+          User Redux Thien Binh Project
+        </div>
+        <div className="user-redux-body">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 my-3">
+                <b>
+                  <FormattedMessage id="manage-user.add" />
+                </b>
+              </div>
+              <div className="col-6">
+                <label>
+                  <FormattedMessage id="manage-user.email" />
+                </label>
+                <input className="form-control" type="email" />
+              </div>
+              <div className="col-6">
+                <label>
+                  <FormattedMessage id="manage-user.password" />
+                </label>
+                <input className="form-control" type="password" />
+              </div>
+              <div className="col-6">
+                <label>
+                  <FormattedMessage id="manage-user.first-name" />
+                </label>
+                <input className="form-control" type="text" />
+              </div>
+              <div className="col-6">
+                <label>
+                  <FormattedMessage id="manage-user.last-name" />
+                </label>
+                <input className="form-control" type="text" />
+              </div>
+              <div className="col-3">
+                <label>
+                  <FormattedMessage id="manage-user.phone-number" />
+                </label>
+                <input className="form-control" type="text" />
+              </div>
+              <div className="col-9">
+                <label>
+                  <FormattedMessage id="manage-user.address" />
+                </label>
+                <input className="form-control" type="text" />
+              </div>
+              <div className="col-3">
+                <label>
+                  <FormattedMessage id="manage-user.gender" />
+                </label>
+                <select className="form-select">
+                  {genders &&
+                    genders.length > 0 &&
+                    genders.map((item, index) => {
+                      return (
+                        <option key={index}>
+                          {language === LANGUAGES.VI
+                            ? item.valueVi
+                            : item.valueEn}
+                        </option>
+                      );
+                    })}
+                </select>
+              </div>
+              <div className="col-3">
+                <label>
+                  <FormattedMessage id="manage-user.position" />
+                </label>
+                <select className="form-select" defaultValue="Choose...">
+                  <option value="Choose...">Choose...</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                  <option value="Other">Other</option>
+                </select>
+              </div>
+              <div className="col-3">
+                <label>
+                  <FormattedMessage id="manage-user.role" />
+                </label>
+                <select className="form-select" defaultValue="Choose...">
+                  <option value="Choose...">Choose...</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                  <option value="Other">Other</option>
+                </select>
+              </div>
+              <div className="col-3">
+                <label>
+                  <FormattedMessage id="manage-user.image" />
+                </label>
+                <input className="form-control" type="text" />
+              </div>
+              <div className="col-12 my-3">
+                <button className="btn btn-primary">
+                  <FormattedMessage id="manage-user.save" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    language: state.app.language,
+    genderRedux: state.admin.genders,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getGenderStart: () => dispatch(actions.fetchGenderStart()),
+    // processLogout: () => dispatch(actions.processLogout()),
+    // changeLanguageAppRedux: (language) =>
+    //   dispatch(actions.changeLanguageApp(language)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserRedux);
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,24 +2,16 @@ import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 
 import appReducer from "./appReducer";
-import adminReducer from "./adminReducer";
 import userReducer from "./userReducer";
-
+import adminReducer from "./adminReducer";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
-import { keys } from "lodash";
 
 const persistCommonConfig = {
   storage: storage,
   stateReconciler: autoMergeLevel2,
 };
-
-// const adminPersistConfig = {
-//   ...persistCommonConfig,
-//   key: "admin",
-//   whitelist: ["isLoggedIn", "adminInfo"],
-// };
 
 const userPersistConfig = {
   ...persistCommonConfig,
@@ -38,4 +30,5 @@ export default (history) =>
     // admin: persistReducer(adminPersistConfig, adminReducer),
     user: persistReducer(userPersistConfig, userReducer),
     app: persistReducer(appPersistConfig, appReducer),
+    admin: adminReducer,
   });
