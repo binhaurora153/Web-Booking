e11e045cd7d736b990c5d0cac0e0307bf78273bd
Thiện Bình <21t1020254@husc.edu.vn>
2024-09-29 14:30:49 +0700

    Customize Data Gửi Email Đặt Lịch Khám Bệnh Với NodeMailer

--- src/containers/Patient/Doctor/Modal/BookingModal.js
+++ src/containers/Patient/Doctor/Modal/BookingModal.js
@@ -11,6 +11,7 @@ import { LANGUAGES } from "../../../../utils";
 import Select from "react-select";
 import { postPatientBookAppointment } from "../../../../services/userService";
 import { toast, Toast } from "react-toastify";
+import moment from "moment";
 class BookingModal extends Component {
   constructor(props) {
     super(props);
@@ -89,9 +90,45 @@ class BookingModal extends Component {
     });
   };
 
+  buildTimeBooking = (dataTime) => {
+    let { language } = this.props;
+    if (dataTime && !_.isEmpty(dataTime)) {
+      let time =
+        language === LANGUAGES.VI
+          ? dataTime.timeTypeData.valueVi
+          : dataTime.timeTypeData.valueEn;
+      let date =
+        language === LANGUAGES.VI
+          ? moment
+              .unix(+dataTime.date / 1000)
+
+              .format("dddd - DD/MM/YYYY")
+          : moment
+              .unix(+dataTime.date / 1000)
+              .locale("en")
+              .format("ddd - MM/DD/YYYY");
+      return ` ${time} - ${date}`;
+    }
+    return "";
+  };
+
+  builDoctorName = (dataTime) => {
+    let { language } = this.props;
+    if (dataTime && !_.isEmpty(dataTime)) {
+      let name =
+        language === LANGUAGES.VI
+          ? `${dataTime.doctorData.lastName} ${dataTime.doctorData.firstName}`
+          : `${dataTime.doctorData.firstName} ${dataTime.doctorData.lastName}`;
+
+      return name;
+    }
+    return "";
+  };
   handleConfirmBooking = async () => {
     //validate input
+    let timeString = this.buildTimeBooking(this.props.dataTime);
     let date = new Date(this.state.birthday).getTime();
+    let doctorName = this.builDoctorName(this.props.dataTime);
     let res = await postPatientBookAppointment({
       fullName: this.state.fullName,
       phoneNumber: this.state.phoneNumber,
@@ -102,6 +139,9 @@ class BookingModal extends Component {
       selectedGender: this.state.selectedGender.value,
       doctorId: this.state.doctorId,
       timeType: this.state.timeType,
+      language: this.props.language,
+      timeString: timeString,
+      doctorName: doctorName,
     });
 
     if (res && res.errCode === 0) {
