4d97c28b542dcf9feb4ab0f8713a9a50751e3c01
Thiện Bình <21t1020254@husc.edu.vn>
2024-09-30 10:16:03 +0700

    Hoàn Thiện Chức Năng Xác Nhận Lịch Hẹn Khám Bệnh Qua Email

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,10 +18,10 @@ import Login from "./Auth/Login";
 
 import System from "../routes/System";
 
-import { CustomToastCloseButton } from "../components/CustomToast";
 import HomePage from "./HomePage/HomePage.js";
 import CustomScrollbars from "../components/CustomScrollbars.js";
 import DetailDoctor from "./Patient/Doctor/DetailDoctor.js";
+import VerifyEmail from "./Patient/VerifyEmail.js";
 class App extends Component {
   handlePersistorState = () => {
     const { persistor } = this.props;
@@ -65,6 +65,11 @@ class App extends Component {
                   />
                   <Route path={path.HOMEPAGE} component={HomePage} />
                   <Route path={path.DETAIL_DOCTOR} component={DetailDoctor} />
+
+                  <Route
+                    path={path.VERIFY_EMAIL_BOOKING}
+                    component={VerifyEmail}
+                  />
                 </Switch>
               </CustomScrollbars>
             </div>
