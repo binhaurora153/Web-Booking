3b42c581cac119416c97ccac8593bc5b29af2d81
Thiện Bình <21t1020254@husc.edu.vn>
2024-09-30 19:36:51 +0700

    Tạo Chức Năng Thêm Chuyên Ngành Khám Bệnh

--- src/routes/System.js
+++ src/routes/System.js
@@ -6,7 +6,7 @@ import UserRedux from "../containers/System/Admin/UserRedux";
 
 import Header from "../containers/Header/Header";
 import ManageDoctor from "../containers/System/Admin/ManageDoctor";
-
+import ManageSpecialty from "../containers/System/Specialty/ManageSpecialty";
 class System extends Component {
   render() {
     {
@@ -21,6 +21,10 @@ class System extends Component {
               <Route path="/system/user-manage" component={UserManage} />
               <Route path="/system/user-redux" component={UserRedux} />
               <Route path="/system/manage-doctor" component={ManageDoctor} />
+              <Route
+                path="/system/manage-specialty"
+                component={ManageSpecialty}
+              />
 
               <Route
                 component={() => {
